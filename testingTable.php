8d92a9cce3121e4942e6139d8fb87222fbd9c668
Afeefnih <afeefz2003@gmail.com>
2024-04-16 00:18:25 +0800

    update test table

--- testingTable.php
+++ testingTable.php
@@ -1,3 +1,4 @@
+
 <?php
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', '');
@@ -43,10 +44,13 @@ $spgroup7 = array(0, 0, 0, 0, 0);
 // Display the table
 echo "<table border='1'>";
 echo "<tr><th>spgroup</th>";
+echo "<th>group</th>";
+
+
 foreach ($diameter_ranges as $range) {
     echo "<th>" . $range[0] . "-" . ($range[1] === null ? "Inf" : $range[1]) . "</th>";
 }
-echo "</tr>";
+echo "<th>total</th></tr>";
 
 // Loop through the results
 while ($row = mysqli_fetch_assoc($r)) {
@@ -143,62 +147,136 @@ while ($row = mysqli_fetch_assoc($r)) {
             }
         }
     }
+   
+    $sumsp1 = array(0,0,0,0,0,0,0);
+    foreach ($spgroup1 as $value) {
+        $sumsp1[0] += $value;
+    }
+    foreach ($spgroup2 as $value) {
+        $sumsp1[1] += $value;
+    }
+    foreach ($spgroup3 as $value) {
+        $sumsp1[2] += $value;
+    }
+    foreach ($spgroup4 as $value) {
+        $sumsp1[3] += $value;
+    }
+    foreach ($spgroup5 as $value) {
+        $sumsp1[4] += $value;
+    }
+    foreach ($spgroup6 as $value) {
+        $sumsp1[5] += $value;
+    }
+    foreach ($spgroup7 as $value) {
+        $sumsp1[6] += $value;
+    }
+
+    $totalD1 = 0;
+    $totalD2 = 0;
+    $totalD3 = 0;
+    $totalD4 = 0;
+    $totalD5 = 0;
+    $totalD6 = 0;
+
+  
+  
+    $totalD1 = array_sum(array_column([$spgroup1, $spgroup2, $spgroup3, $spgroup4, $spgroup5,$spgroup6,$spgroup7], 0));
+    $totalD2 = array_sum(array_column([$spgroup1, $spgroup2, $spgroup3, $spgroup4, $spgroup5,$spgroup6,$spgroup7], 1));
+    $totalD3 = array_sum(array_column([$spgroup1, $spgroup2, $spgroup3, $spgroup4, $spgroup5,$spgroup6,$spgroup7], 2));
+    $totalD4 = array_sum(array_column([$spgroup1, $spgroup2, $spgroup3, $spgroup4, $spgroup5,$spgroup6,$spgroup7], 3));
+    $totalD5 = array_sum(array_column([$spgroup1, $spgroup2, $spgroup3, $spgroup4, $spgroup5,$spgroup6,$spgroup7], 4));
+    $total = array_sum($sumsp1);
+
+
+
+
+
+
+
+
 
 echo "<tr>";
 echo "<td>group 1</td>";
+echo "<td>mersawa</td>";
 echo "<td>$spgroup1[0]</td>";
 echo "<td>$spgroup1[1]</td>";
 echo "<td>$spgroup1[2]</td>";
 echo "<td>$spgroup1[3]</td>";
 echo "<td>$spgroup1[4]</td>";
+echo "<td>$sumsp1[0]</td>";
+
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 2</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup2[0]</td>";
 echo "<td> $spgroup2[1]</td>";
 echo "<td> $spgroup2[2]</td>";
 echo "<td> $spgroup2[3]</td>";
 echo "<td> $spgroup2[4]</td>";
+echo "<td>$sumsp1[1]</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 3</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup3[0]</td>";
 echo "<td> $spgroup3[1]</td>";
 echo "<td> $spgroup3[2]</td>";
 echo "<td> $spgroup3[3]</td>";
 echo "<td> $spgroup3[4]</td>";
+echo "<td>$sumsp1[2]</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 4</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup4[0]</td>";
 echo "<td> $spgroup4[1]</td>";
 echo "<td> $spgroup4[2]</td>";
 echo "<td> $spgroup4[3]</td>";
 echo "<td> $spgroup4[4]</td>";
+echo "<td>$sumsp1[3]</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 5</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup5[0]</td>";
 echo "<td> $spgroup5[1]</td>";
 echo "<td> $spgroup5[2]</td>";
 echo "<td> $spgroup5[3]</td>";
 echo "<td> $spgroup5[4]</td>";
+echo "<td>$sumsp1[4]</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 6</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup6[0]</td>";
 echo "<td> $spgroup6[1]</td>";
 echo "<td> $spgroup6[2]</td>";
 echo "<td> $spgroup6[3]</td>";
 echo "<td> $spgroup6[4]</td>";
+echo "<td>$sumsp1[5]</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>group 7</td>";
+echo "<td>mersawa</td>";
 echo "<td> $spgroup7[0]</td>";
 echo "<td> $spgroup7[1]</td>";
 echo "<td> $spgroup7[2]</td>";
 echo "<td> $spgroup7[3]</td>";
 echo "<td> $spgroup7[4]</td>";
+echo "<td>$sumsp1[6]</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Total</td>";
+echo "<td></td>";
+echo "<td>$totalD1</td>";
+echo "<td>$totalD2</td>";
+echo "<td>$totalD3</td>";
+echo "<td>$totalD4</td>";
+echo "<td>$totalD5</td>";
+echo "<td>$total</td>";//3500 
+
 echo "</tr>";
 
 echo "</table>";
@@ -206,3 +284,4 @@ echo "</table>";
 // Close the database connection
 mysqli_close($dbc);
 ?>
+
